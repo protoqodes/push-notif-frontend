8d9c25d6b0741a23c1e05a2946d3faad192cc839
enchantress <enchantress.protoqodes@gmail.com>
2018-01-22 00:39:19 +0800

    update button

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,6 +1,6 @@
 const config = {
-  // baseUrl : 'http://localhost:5016/api',
-  baseUrl : 'https://angeles-notif.herokuapp.com/api'
+  baseUrl : 'http://localhost:5016/api',
+  // baseUrl : 'https://angeles-notif.herokuapp.com/api'
 }
 export default config;
 //
